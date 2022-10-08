3dadd450af869ad6ebff59e8d2aec0c1f1bef60c
Владислав <v.konykhov@yandex.ru>
2022-10-08 13:36:02 +0300

    Файлы адаптировались

---
+++ src/test/java/ru/yandex/practicum/catsgram/CatsgramApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.catsgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
